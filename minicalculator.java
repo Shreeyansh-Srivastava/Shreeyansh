2c1f2a5f3191f1527542005363c2b4d7cf1ab2f7
Shreeyansh-Srivastava <91000842+Shreeyansh-Srivastava@users.noreply.github.com>
2021-09-26 15:58:57 +0530

    Add files via upload

---
+++ minicalculator.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class minicalculator {
+    public static void main(String[] args)
+    {
+        String doing;
+        String a="     sachin     ";
+        String b="     TENDULkar     ";
+        doing="             hello               ";
+        System.out.println("\n"+doing.trim());
+    }
+}
